603371be55a13f369a67a5fa9390fe904a6e74f5
Melissa Hart <melissahart@melissas-air.main.local.una.edu>
2024-03-04 12:38:39 -0600

    k

--- app.js
+++ app.js
@@ -10,12 +10,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// use res.render to load up an ejs view file
-
-let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.URI, {
+const client = new MongoClient(process.env.MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -23,54 +19,45 @@ const client = new MongoClient(process.env.URI, {
   }
 });
 
-async function run() {
+async function mongoConnect() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
-
-    //console.log("cxnDB result: ", result);
+    const result = await client.db("mels-books").collection("books-collection").find().toArray();
+    console.log("cxnDB result: ", result);
 
     return result; 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-//run().catch(console.dir);
 
-app.get('/read', async (req,res) => {
+app.get('/', async (req, res) => {
 
-  let myResultServer = await run(); 
+  let result = await mongoConnect(); 
 
-  console.log("myResultServer:", myResultServer);
-
-  res.render('index', {
-    myTypeClient: myTypeServer,
-    myResultClient: myResultServer
-
-  });
-
-
-}); 
-run().catch(console.dir);
-
-
-
-app.get('/', function(req, res) {
+  console.log("cxnDB result: ", result);
 
   res.render('index', {
    
-    myTypeClient: myTypeServer 
+    pageTitle: 
+    bookData: result
 
   });
   
 });
+
+
+
+
+
+
+
 
 app.get('/name', (req,res) => {
 
